96867339a96217e81f94f115296c8a7b56a50fca
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-08-03 19:50:02 -0300

    implementing news feed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,18 @@
 import { Header } from '@/components/header'
 import { NewsFeed } from './components/news-feed'
+import { useEffect } from 'react'
+import { api } from '@/services/web/api'
 
 export default function Home() {
+  async function handleGetNews() {
+    await api.get('/news/get-news')
+  }
+
   return (
     <>
       <Header />
-      <NewsFeed />
+      <button onClick={handleGetNews}>chamar</button>
+      {/* <NewsFeed /> */}
     </>
   )
 }
