f5d4b74dd7345a250fce6f2eb97a86c2f60b8461
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-08-08 10:36:03 -0300

    fex header semantic

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -3,18 +3,14 @@ import Link from 'next/link'
 export function HeaderNav() {
   return (
     <nav className="text-md flex h-8 items-center text-sm font-medium text-white">
-      <div className="flex h-8 items-center gap-2 ">
-        <Link href={'/'}>
-          <span className="relative h-full after:absolute after:bottom-0 after:left-0 after:hidden after:h-[1px] after:w-full after:bg-white hover:after:block">
-            Home
-          </span>
-        </Link>
-        <Link href={'/custom-news-feed'}>
-          <span className="relative h-full after:absolute after:bottom-0 after:left-0 after:hidden after:h-[1px] after:w-full after:bg-white hover:after:block">
-            News feed
-          </span>
-        </Link>
-      </div>
+      <ul className="flex h-8 items-center gap-2 ">
+        <li className="relative flex h-full items-center after:absolute after:bottom-0 after:left-0 after:hidden after:h-[1px] after:w-full after:bg-white hover:after:block">
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li className="relative flex h-full items-center after:absolute after:bottom-0 after:left-0 after:hidden after:h-[1px] after:w-full after:bg-white hover:after:block">
+          <Link href={'/custom-news-feed'}>News feed</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
