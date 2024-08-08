beae5486e931d97627b6553670762ffe9c5767f7
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-08-08 15:46:48 -0300

    fix lint problem

--- src/components/header/modal-search.test.tsx
+++ src/components/header/modal-search.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { ModalSearch } from './modal-search'
 import { userEvent } from '@testing-library/user-event'
 import { Dialog } from '../ui/dialog'
